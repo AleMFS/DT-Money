b116f0fbe6fb34481f97b3fd1743800b08860800
Alexandre Santos <Alexandre_MFS@outlook.com>
2023-01-10 19:28:14 -0300

    adicionado json-server e formatação de numeros e datas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from 'styled-components'
+import { TransactionsProvider } from './contexts/TransactionsContexts'
 import { Transactions } from './pages/Transactions'
 import { GlobalStyle } from './styles/globa'
 import { defaultTheme } from './styles/themes/default'
@@ -9,9 +10,10 @@ export default function App() {
   return (
 
     <ThemeProvider theme={defaultTheme}>
-      
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
 
-      <Transactions />
       <GlobalStyle />
     </ThemeProvider>
 
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,16 @@
+import { useContext, useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { TransactionsContext } from "../../contexts/TransactionsContexts";
+import { dateFormatter, priceFormmater } from "../../utils/formatter";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
+
+
 export function Transactions() {
+    const { transactions } = useContext(TransactionsContext)
+
     return (
         <div>
             <Header />
@@ -13,28 +20,22 @@ export function Transactions() {
                 <SearchForm />
                 <TransactionsTable>
                     <tbody>
-                        <tr>
-                            <td width='50%'>Desenvolvimento de site </td>
-                            <td>
-                                <PriceHighLight variant="income">
-                                    R$12.000,00
-                                </PriceHighLight>
-                            </td>
+                        {transactions.map(transaction => {
+                            return (
+                                <tr key={transaction.id}>
+                                    <td width='50%'>{transaction.description} </td>
+                                    <td>
+                                        <PriceHighLight variant={transaction.type}>
+                                            {transaction.type === 'outcome' && '- '}
+                                            {priceFormmater.format(transaction.price)}
+                                        </PriceHighLight>
+                                    </td>
 
-                            <td>Venda</td>
-                            <td>09/01/2023</td>
-                        </tr>
-                        <tr>
-                            <td width='50%'>Desenvolvimento de site </td>
-                            <td>
-                                <PriceHighLight variant="outcome">
-                                    - R$12.000,00
-                                </PriceHighLight>
-                            </td>
-                            <td>Venda</td>
-                            <td>09/01/2023</td>
-                        </tr>
-
+                                    <td>{transaction.category}</td>
+                                    <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
+                                </tr>
+                            )
+                        })}
                     </tbody>
                 </TransactionsTable>
             </TransactionsContainer>
